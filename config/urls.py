86b1bdd9e1a54816acb1a61b6090ad42a3ebf9ab
yutanyan <yutanyan@yahoo.co.jp>
2019-10-16 23:40:04 +0900

    191016-1

--- config/urls.py
+++ config/urls.py
@@ -2,5 +2,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('common_page.urls')),
-    path('de_kensaku/', include('de_kensaku.urls')),
+    path('de-kensaku/', include('de_kensaku.urls')),
 ]
