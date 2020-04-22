35492bfa5d402b20cbac95ce02b4b85473e84ac7
yutanyan <yutanyan@yahoo.co.jp>
2020-04-22 19:27:30 +0900

    2020年4月22日

--- common_page/views.py
+++ common_page/views.py
@@ -4,14 +4,14 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
 
-def owner(request):
-    template = loader.get_template("common_page/owner.html")
-    return HttpResponse(template.render({}, request))
-
 def toppage(request):
-    template = loader.get_template("common_page/toppage.html")
-    return HttpResponse(template.render({}, request))
+		template = loader.get_template("common_page/toppage.html")
+		return HttpResponse(template.render({}, request))
+
+def owner(request):
+		template = loader.get_template("common_page/owner.html")
+		return HttpResponse(template.render({}, request))
 
 def updated(request):
-    template = loader.get_template("common_page/updated.html")
-    return HttpResponse(template.render({}, request))
+		template = loader.get_template("common_page/updated.html")
+		return HttpResponse(template.render({}, request))
