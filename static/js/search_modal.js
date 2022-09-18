e66c12ebb562fe5d855307f960c519d5f31fcaa8
dev-neko <dev-neko@dev.neko>
2022-09-18 16:39:40 +0900

    commit

--- static/js/search_modal.js
+++ static/js/search_modal.js
@@ -1,4 +1,6 @@
+//https://www.web-dev-qa-db-ja.com/ja/javascript/iframeをbootstrap-modalで読み込みます/1048168472/
 $('#Bing').on('click',function(e){
+	//これを入れないとモーダルを開いて別タブでも開いてしまう
 	e.preventDefault();
 	$('#Bing_modal').on('shown.bs.modal',function() {
 		$(this).find('iframe').attr('src','https://www.bing.com/search?q='+$('#target').val())
