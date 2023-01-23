e87dc233a5a659fe912799fd852243c519f716c7
dev-neko <dev-neko@dev.neko>
2023-01-23 10:17:51 +0900

    2023年1月23日

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('', include('common_page.urls')),
-	path('dev-fd36c01ask/',include('dev.urls')),
+	# path('dev-fd36c01ask/',include('dev.urls')),
 	path('de_kensaku/',include('de_kensaku.urls')),
 	path('support-parts-gen/',include('support_parts_gen.urls')),
 ]
